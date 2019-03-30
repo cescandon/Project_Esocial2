714a06d120051275a80d0b50d3b0f14a11078c7f
Francisco Diaz <46197605+NullByt3s@users.noreply.github.com>
2019-03-29 22:48:00 -0700

    Updating

---
+++ app/src/main/java/com/project/esocial/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.project.esocial;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText textUsername, textPassword;
+    private Button btnLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        userIsLoggedIn();
+
+        textUsername = findViewById(R.id.inputUsername);
+        textPassword = findViewById(R.id.inputUserpassword);
+        btnLogin = findViewById(R.id.userLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                    System.out.println("onclick code");
+                    verifyUserLogin();
+
+            }
+
+        });
+
+    }
+
+    private void verifyUserLogin(){
+        System.out.println("Loging credentials " + textUsername.getText() + " and password " + textPassword.getText());
+    }
+
+    private void userIsLoggedIn() {
+
+    }
+
+}
---
+++ app/src/main/java/com/project/esocial/PhoneLoginActivity.java
@@ -0,0 +1,48 @@
+package com.project.esocial;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class PhoneLoginActivity extends AppCompatActivity {
+
+
+    private EditText mPhoneNumber, mCode;
+    private Button mSend;
+
+
+    //private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    String mVerificationId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
